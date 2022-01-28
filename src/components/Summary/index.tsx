2c5896911eea4529a58d6175ef0993328ef63736
AndreSDS <andre_smiths@outlook.com>
2022-01-28 20:51:54 -0300

    Criando componente Summary

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,33 @@
+import incomeImg from "../../assets/entradas.svg";
+import outcomeImg from "../../assets/saidas.svg";
+import totalImg from "../../assets/total.svg";
+
+import { Container } from "./styles";
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeImg} alt="entradas" />
+        </header>
+        <strong>+ R$1.000,00</strong>
+      </div>
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={outcomeImg} alt="entradas" />
+        </header>
+        <strong>- R$1.000,00</strong>
+      </div>
+      <div className="highlight">
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="entradas" />
+        </header>
+        <strong>R$ 2.000,00</strong>
+      </div>
+    </Container>
+  );
+}
