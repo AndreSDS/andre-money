37a3fe92c0b892a3575c400fb08f1ea91aff18e3
AndreSDS <andre_smiths@outlook.com>
2022-01-30 01:58:53 -0300

    importando e usando o componente modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
 import { GlobalStyle } from "./styles/global";
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
 export function App() {
+  const [isNewTransactionModal, setIsNewTransactionModal] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModal(true);
+  }
+
+  function handleCloseNewlTransactions() {
+    setIsNewTransactionModal(false);
+  }
+
   return (
     <>
       <GlobalStyle />
-      <Header />
+      <NewTransactionModal
+        isOpen={isNewTransactionModal}
+        onRequestClose={handleCloseNewlTransactions}
+      />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
     </>
   );
