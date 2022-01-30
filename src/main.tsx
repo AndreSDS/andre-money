e283d56a45a7557a756a161024a033fa518f4db1
AndreSDS <andre_smiths@outlook.com>
2022-01-30 01:53:08 -0300

    criando instalando e configurando miragejs

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {createServer} from 'miragejs'
 import {App} from './App'
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Desenvolvimento de web site',
+          amount: 12000,
+          category: 'Desenvolvimento',
+          created_at: new Date()
+        },
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
