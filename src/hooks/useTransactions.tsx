39af359a7ec99f3fd4c62bebdb7324e399a95709
andresds <andre_smiths@outlook.com>
2022-07-20 13:23:42 -0300

    refatorando state global para usar zustand

--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,51 +1,32 @@
-import React, {
-  createContext,
-  useEffect,
-  useState,
-  ReactNode,
-  useContext,
-} from "react";
 import { api } from "../services/api";
 import { TransactionProps, TransactionInput } from "../interfaces";
-
-interface TransactionsProviderProps {
-  children: ReactNode;
-}
+import create from "zustand";
 
 interface TransactionsContextProps {
   transactions: TransactionProps[];
   createTransaction: (transaction: TransactionInput) => Promise<void>;
+  getAllTransactions: () => Promise<void>;
 }
 
-const TransactionsContext = createContext<TransactionsContextProps>(
-  {} as TransactionsContextProps
-);
-
-export function TransactionsProvider({ children }: TransactionsProviderProps) {
-  const [transactions, setTransactions] = useState<TransactionProps[]>([]);
-
-  async function createTransaction(transactionInput: TransactionInput) {
+export const useTransactions = create<TransactionsContextProps>((set) => ({
+  transactions: [],
+  createTransaction: async (transactionInput) => {
     const response = await api.post("/transactions", transactionInput);
     const { transaction } = response.data;
 
-    setTransactions([...transactions, transaction]);
-  }
+    return set((store) => ({
+      ...store,
+      transactions: [...store.transactions, transaction],
+    }));
+  },
+  getAllTransactions: async () => {
+    const {
+      data: { transactions },
+    } = await api.get("/transactions");
 
-  useEffect(() => {
-    api.get("transactions").then((response) => {
-      setTransactions(response.data.transactions);
-    });
-  }, []);
-
-  return (
-    <TransactionsContext.Provider value={{ transactions, createTransaction }}>
-      {children}
-    </TransactionsContext.Provider>
-  );
-}
-
-export function useTransactions() {
-  const context = useContext(TransactionsContext);
-
-  return context;
-}
+    return set((store) => ({
+      ...store,
+      transactions: [...transactions],
+    }));
+  },
+}));
