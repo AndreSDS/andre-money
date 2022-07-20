3873ae1260274ae35b7dddd7bf9dfb0da28657ae
andresds <andre_smiths@outlook.com>
2022-07-20 13:22:30 -0300

    movendo a funcção que sobre o servidor para outro arquivo

--- src/main.tsx
+++ src/main.tsx
@@ -1,54 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {createServer, Model} from 'miragejs'
-import {App} from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./App";
+import { createMockServer } from "./services/server";
 
-createServer({
-  models: {
-    transaction: Model
-  },
-
-  seeds(server) {
-    server.db.loadData({
-      transactions: [
-        {
-          id: 1,
-          title: 'Desenvolvimento de web site',
-          type: 'deposit',
-          category: 'Desenvolvimento',
-          amount: 12000,
-          createdAt: new Date('2021-02-14 09:00:00')
-        },
-        {
-          id: 2,
-          title: 'Aluguel',
-          type: 'withdraw',
-          category: 'Casa',
-          amount: 1000,
-          createdAt: new Date('2021-02-20 09:00:00')
-        }
-      ]
-    })
-  },
-
-  routes() {
-    this.namespace = 'api';
-
-    this.get('/transactions', () => {
-      return this.schema.all('transaction');
-    })
-
-    this.post('/transactions', (schema, request) => {
-      let data = JSON.parse(request.requestBody);
-      data = {...data, createdAt: new Date()};
-      return schema.create('transaction', data);
-    })
-  }
-})
+// inicia o server mock com miragejs
+createMockServer();
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
